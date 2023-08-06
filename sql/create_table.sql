8c68f3982bbf2fdaa3405fc9264e1ea80e148ac2
Fred1um <104199670+Fred1um@users.noreply.github.com>
2023-08-06 21:13:38 +0400

    Create create_table.sql

---
+++ sql/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `taxi_data` (
+  `VendorID` int,
+  `passenger_count` int,
+  `trip_distance` float,
+  `RatecodeID` float,
+  `payment_type` int,
+  `total_amount` float);
